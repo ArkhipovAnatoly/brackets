8579edb3549c60079e917307f1bf098f4a5edd27
Anatoly Arkhipov <atol83@mail.ru>
2021-08-28 22:13:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bObj = {};
+  const stack = [];
+
+  bracketsConfig.forEach((value) => (bObj[value[0]] = value[1]));
+
+  for (let i = 0; i < str.length; i++) {
+    if (bObj[stack[stack.length - 1]] === str[i]) {
+      stack.pop();
+    } else if (str[i] in bObj) {
+      stack.push(str[i]);
+    } else {
+      return false;
+    }
+  }
+
+  if (stack.length !== 0) return false;
+
+  return true;
+};
